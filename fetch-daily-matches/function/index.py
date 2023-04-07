968a65b10eee5b5b08b6a5ef2b18f508adfa8d70
miztch <miztch.me@gmail.com>
2023-04-08 00:37:44 +0900

    separated region map as a json file

--- fetch-daily-matches/function/index.py
+++ fetch-daily-matches/function/index.py
@@ -25,6 +25,16 @@ def insert(table, match_list):
             batch.put_item({k: v for k, v in match.items()})
 
 
+def load_json(file_name):
+    '''
+    get json object from file and return as dict
+    '''
+    with open(file_name) as file:
+        dict = json.load(file)
+
+    return dict
+
+
 def is_json(json_str):
     '''
     judge if json_str is valid.
@@ -92,28 +102,19 @@ def calc_match_end_time(start_time, best_of):
     return end_time_str, ttl
 
 
-def map_flag_to_region(flag):
+def map_flag_to_region(flag, region_map):
     '''
     get flag(usually it shows country) indicator and return region
     '''
 
-    region_map = {
-        'au': 'APAC', 'ar': 'BR_LATAM', 'at': 'EMEA', 'ba': 'EMEA', 'be': 'EMEA', 'br': 'BR_LATAM',
-        'ca': 'NA', 'ch': 'EMEA', 'cl': 'BR_LATAM', 'cn': 'EAST_ASIA', 'cz': 'EMEA', 'de': 'EMEA',
-        'dk': 'EMEA', 'ee': 'EMEA', 'eg': 'EMEA', 'es': 'EMEA', 'fi': 'EMEA', 'fr': 'EMEA',
-        'gb': 'EMEA', 'gr': 'EMEA', 'hk': 'APAC', 'hu': 'EMEA', 'hr': 'EMEA', 'id': 'APAC',
-        'ie': 'EMEA', 'il': 'EMEA', 'in': 'APAC', 'iq': 'EMEA', 'is': 'EMEA', 'it': 'EMEA', 'jp': 'EAST_ASIA',
-        'kh': 'APAC', 'kr': 'EAST_ASIA', 'kw': 'EMEA', 'lt': 'EMEA', 'ma': 'EMEA', 'me': 'EMEA', 'mk': 'EMEA', 'my': 'APAC',
-        'no': 'EMEA', 'pe': 'BR_LATAM', 'ph': 'APAC', 'pl': 'EMEA', 'pt': 'EMEA', 'ro': 'EMEA',
-        'rs': 'EMEA', 'sa': 'APAC', 'se': 'EMEA', 'sg': 'APAC', 'si': 'EMEA', 'th': 'APAC',
-        'tr': 'EMEA', 'tw': 'APAC', 'ua': 'EMEA', 'us': 'NA', 'vn': 'APAC',
-        'asia-pacific': 'APAC', 'benelux': 'EMEA', 'cis': 'EMEA', 'dach': 'EMEA',
-        'east-asia': 'EAST_ASIA', 'eu': 'EMEA', 'latam': 'BR_LATAM', 'nordic': 'EMEA',
-        'oce': 'APAC', 'south-asia': 'APAC', 'southeast-asia': 'APAC', 'usa-ca': 'NA', 'world': 'EMEA'
-    }
+    map = [country for country in region_map if country['country_code'] == flag]
 
-    if flag in region_map:
-        region = region_map[flag]
+    if map:
+        region = map[0]['region']
+    elif len(map) >= 2:
+        logger.warning(
+            "region map returned multiple records for the flag: {}".format(flag))
+        region = ''
     else:
         logger.warning("no region map has found for the flag: {}".format(flag))
         region = ''
@@ -125,7 +126,8 @@ def fetch_daily_matches(date):
     '''
     fetch match information for the specified day from api endpoint
     '''
-
+    # configure datasource
+    region_map = load_json("./countries.json")
     endpoint = "https://api.thespike.gg/matches"
 
     logger.info('fetch matches list from: {}'.format(endpoint))
@@ -156,7 +158,7 @@ def fetch_daily_matches(date):
         # get region from flag indicator
         # skip if empty so that "Calendar Id" for Gcal cannot be determined
         flag = match['eventCountryFlag']
-        region = map_flag_to_region(flag)
+        region = map_flag_to_region(flag, region_map)
 
         if not region:
             continue
