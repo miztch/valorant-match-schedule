a3fe2d9cfc91235020d4ef52dfc30595fd4bda6d
miztch <8962490+miztch@users.noreply.github.com>
2024-08-05 04:09:14 +0900

    feat: add calendars of divisions for international regions.

--- function/fetch-daily-matches/constants.py
+++ function/fetch-daily-matches/constants.py
@@ -1,3 +1,12 @@
+tier1_regions = {
+    "NA": "AMERICAS",
+    "BR_LATAM": "AMERICAS",
+    "APAC": "PACIFIC",
+    "EAST_ASIA": "PACIFIC",
+    "CHINA": "CHINA",
+    "EMEA": "EMEA",
+}
+
 countries = {
     "ar": "BR_LATAM",
     "at": "EMEA",
@@ -9,7 +18,7 @@ countries = {
     "ca": "NA",
     "ch": "EMEA",
     "cl": "BR_LATAM",
-    "cn": "EAST_ASIA",
+    "cn": "CHINA",
     "co": "BR_LATAM",
     "cz": "EMEA",
     "de": "EMEA",
