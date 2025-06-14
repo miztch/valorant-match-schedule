b9a85a67f5e7ae47edd0799461bb545fe555568f
miztch <miztch.me@gmail.com>
2025-06-14 19:03:52 +0900

    fix: Valorant Masters Toronto 2025

--- function/fetch-daily-matches/constants.py
+++ function/fetch-daily-matches/constants.py
@@ -154,7 +154,7 @@ international_events = [
     "Champions Tour 2025: China Kickoff",
     "Champions Tour 2025: Masters Bangkok",
     "Champions Tour 2025: Masters Toronto",
-    "Valorant Masters 2025: Toronto",
+    "Valorant Masters Toronto 2025",
     "Champions Tour 2025: China Stage 1",
     "Champions Tour 2025: Americas Stage 1",
     "Champions Tour 2025: EMEA Stage 1",
